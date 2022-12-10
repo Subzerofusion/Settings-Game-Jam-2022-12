d57f0aba384c8ed418f08ca28d96e221592f47c2
Mika Heares <mheares@idea11.com.au>
2022-12-10 18:17:13 +1000

    updated styles to look less shit, moving changes sprite rotation

--- Mika/Scripts/Player.gd
+++ Mika/Scripts/Player.gd
@@ -7,18 +7,19 @@ func _process(delta):
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
-
+	
 	if Input.is_action_pressed("ui_left"):
+		rotation = PI
 		direction.x = -1
 	elif Input.is_action_pressed("ui_right"):
+		rotation = 0 
 		direction.x = 1
 	if Input.is_action_pressed("ui_up"):
+		rotation = PI * 1.5
 		direction.y = -1
 	elif Input.is_action_pressed("ui_down"):
+		rotation = PI * .5
 		direction.y = 1
-
-	var updated_pos = (direction * delta * speed)
 	
+	var updated_pos = (direction * delta * speed)
 	var collision = move_and_collide(updated_pos)
-	if (collision):
-		print(collision)
